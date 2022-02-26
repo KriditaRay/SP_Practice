3437f1a56cfd4aa772a02b2b8d1e248ec32292bf
KriditaRay <99970517+KriditaRay@users.noreply.github.com>
2022-02-27 03:37:05 +0600

    JS started

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("I am Kridita Ray.")
+var name = 'Kridita Ray';
+let age = 22;
+const BG = 'B+'
+
+console.log('My name is ' + name + '. My age is ' + age + '. My Blood group is ' + BG + '.');
+
+const car = {
+    wheels: 4,
+    color: 'Black',
+    'max speed': 400
+}
+
+car.model = 'toyota';
+
+
+console.log('Name of our car is ' + car['max speed']);
